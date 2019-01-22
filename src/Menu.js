aa01a83ec3ccaf9f442be281f41dfd95466cec7e
cassinodari <cassi.nodari@hotmail.com>
2019-01-21 23:42:45 -0200

    Flex Box

--- src/Menu.js
+++ src/Menu.js
@@ -11,9 +11,14 @@ import ValidarProps from './componentes/ValidarProps'
 import Evento from './componentes/Evento'
 import Avo from './componentes/ComunicacaoDireta'
 import TextoSincronizado from './componentes/ComunicacaoIndireta'
+import ListaFlex from './componentes/ListaFlex'
 
 export default createDrawerNavigator ({
 //Instanciando funcao e classe
+    ListaFlex: {
+        screen: ListaFlex,
+        navigationOptions: { title: 'Lista Flex (Flex Box)'}
+    },
     TextoSincronizado: {
         screen: TextoSincronizado,
         navigationOptions: { title: 'Texto Sincronizado'}
